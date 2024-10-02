003072c783d60e5a759986eeb60ebc8c2f22f0e8
Peyton Comer <pcomer2@huskers.unl.edu>
2024-10-02 18:55:40 -0500

    css experimentation

--- src/Components/Navbutton/Navbutton.jsx
+++ src/Components/Navbutton/Navbutton.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Navbutton.css";
 
-const Navbutton = (path, text
-) => {
-    const navigate = useNavigate();
-    return (
-        <button className="Navbutton" onClick={() => navigate(path)}>{text}</button>
-    )
-}
+const Navbutton = ({ path, text }) => {
+  const navigate = useNavigate();
+  return <button className="Navbutton" onClick={() => navigate(path)}>{text}</button>;
+};
 
-export default Navbutton
+export default Navbutton;
